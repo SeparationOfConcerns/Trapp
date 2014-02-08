167d89b4321dbd431b30e1e73ed84da8414eaea4
SeparationOfConcerns <separationofconcerns@gmail.com>
2014-02-08 12:23:20 +0000

    Updated trees

--- scripts/trees.js
+++ scripts/trees.js
@@ -1,8 +1,10 @@
-function loadTrees(leafShape, buds, fruits, stemColor)
+function loadTrees()
 {
 	var trees = new Array();
 	
 	trees[0]={name:"Dogwood",latinname:"Cornus sanguinea",leafshape:"Oval, curved veins",buds:"Opposite", fruits:"Fleshy", stemcolor: "Red", fruitcolor: "", photo: "dogwood.jpg"};
 	
 	trees[1]={name:"English Elm",latinname:"Ulmus procera",leafshape:"Oval",buds:"Alternate", fruits:"Hard", stemcolor: "", fruitcolor: "", photo: "englishelm.jpg"};
+	
+	return trees;
 }
