14a80a6859bf104d85e4569990253c4ffe81f50e
SeparationOfConcerns <separationofconcerns@gmail.com>
2014-02-08 12:35:36 +0000

    Update trees

--- scripts/trees.js
+++ scripts/trees.js
@@ -2,9 +2,9 @@ function loadTrees()
 {
 	var trees = new Array();
 	
-	trees[0]={name:"Dogwood",latinname:"Cornus sanguinea",leafshape:"Oval, curved veins",buds:"Opposite", fruits:"Fleshy", stemcolor: "Red", fruitcolor: "", photo: "dogwood.jpg"};
+	trees[0]={name:"Dogwood",latinname:"Cornus sanguinea",leafshape:"Oval, curved veins",buds:"Opposite", fruittype:"Fleshy", stemcolor: "Red", fruitcolor: "", photo: "dogwood.jpg"};
 	
-	trees[1]={name:"English Elm",latinname:"Ulmus procera",leafshape:"Oval",buds:"Alternate", fruits:"Hard", stemcolor: "", fruitcolor: "", photo: "englishelm.jpg"};
+	trees[1]={name:"English Elm",latinname:"Ulmus procera",leafshape:"Oval",buds:"Alternate", fruittype:"Hard", stemcolor: "", fruitcolor: "", photo: "englishelm.jpg"};
 	
 	return trees;
 }
