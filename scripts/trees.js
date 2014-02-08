b736fdaf60740e61085fc6405a34b89782cb3cde
jmarcelij <marcelij@tcd.ie>
2014-02-08 17:01:47 +0000

    Update trees.js

--- scripts/trees.js
+++ scripts/trees.js
@@ -6,5 +6,39 @@ function loadTrees()
 	
 	trees[1]={name:"English Elm",latinname:"Ulmus procera",leafshape:"Oval",buds:"Alternate", fruittype:"Hard", stemcolor: "", fruitcolor: "", photo: "englishelm.jpg"};
 	
+	trees[3]={name:"Yew",latinname:"Taxus baccata",leafshape:"needle-like",buds:"", fruittype:"fleshy", stemcolor: "", fruitcolor: "", photo: "yew.jpg"};
+
+	trees[4]={name:"European Larch ",latinname:"Larix decidua",leafshape:"Short needle-like",buds:"", fruittype:"Cone", stemcolor: "", fruitcolor: "", photo: "europeanlarch.jpg"};
+
+	trees[5]={name:"Scots Pine",latinname:"Pinus sylvestris",leafshape:"Long needle-like",buds:"", fruittype:"Cone", stemcolor: "", fruitcolor: "", photo: "Scotspine.jpg"};
+
+	trees[6]={name:"Ivy",latinname:"Hedera helix",leafshape:"Hand-shaped",buds:"alternate", fruittype:"Fleshy", stemcolor: "", fruitcolor: "Black", photo: "Ivy.jpg"};
+
+	trees[7]={name:"Hawthorn",latinname:"Crataegus monogyna",leafshape:"Hand-shaped",buds:"alternate", fruittype:"Fleshy.", stemcolor: "", fruitcolor: "Red", photo: "hawthorn.jpg"};
+
+	trees[8]={name:"Maple",latinname:"Acer campestre",leafshape:"Hand-shaped",buds:"opposite", fruittype:"Winged", stemcolor: "", fruitcolor: "", photo: "maple.jpg"};
+
+	trees[9]={name:"Sycomore",latinname:"Acer pseudoplatanus",leafshape:"hand-shaped",buds:"opposite", fruittype:"Winged", stemcolor: "", fruitcolor: "", photo: "sycomore.jpg"};
+
+	trees[10]={name:"Birch",latinname:"Betula pendula",leafshape:"Triangular",buds:"Alternate", fruittype:"", stemcolor: "", fruitcolor: "", photo: "Birch.jpg"};
+
+	trees[11]={name:"Oak",latinname:"Quercy robur",leafshape:"Lobed",buds:"alternate", fruittype:"Hard", stemcolor: "", fruitcolor: "Green", photo: "oak.jpg"};
+
+	trees[12]={name:"Blackberry",latinname:"Rubus fruticosus",leafshape:"Separate leaflets",buds:"Alternate", fruittype:"Berries", stemcolor: "", fruitcolor: "Black", photo: "blackberry.jpg"};
+
+	trees[13]={name:"Horse Chestnut",latinname:"Aescutus hippocastanum",leafshape:"Separate leaflets",buds:"Opposite", fruittype:"Hard", stemcolor: "", fruitcolor: "Brown", photo: "horsechesnut.jpg"};
+	
+	trees[14]={name:"Holly",latinname:"Ilex aquifolium",leafshape:"Oval,spine",buds:"Alternate", fruittype:"Fleshy", stemcolor: "", fruitcolor: "Red", photo: "holly.jpg"};
+
+	trees[15]={name:"Beech",latinname:"Fagus sylvatica",leafshape:"Oval",buds:"Alternate", fruittype:"Hard", stemcolor: "", fruitcolor: "", photo: "beech.jpg"};
+
+	trees[16]={name:"Hazel",latinname:"Corylus avellana",leafshape:"Oval, rounded",buds:"Alternate", fruittype:"Hard", stemcolor: "", fruitcolor: "Brown", photo: "hazel.jpg"};
+
+	trees[17]={name:"Cherry",latinname:"Prunus",leafshape:"Oval, long",buds:"Alternate", fruittype:"Fleshy", stemcolor: "", fruitcolor: "Red", photo: "cherry.jpg"};
+
+	trees[18]={name:"Crack Willow",latinname:"Salix fragilis",leafshape:"Oval, long",buds:"Alternate", fruittype:"", stemcolor: "", fruitcolor: "", photo: "crackwillow.jpg"};
+
+	trees[19]={name:"Evergreen Oak",latinname:"Quercus ilex",leafshape:"Oval, long",buds:"Alternate", fruittype:"Hard", stemcolor: "", fruitcolor: "Green", photo: "evergreenoak.jpg"};
+
 	return trees;
 }
